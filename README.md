4b172a1937e5a93423c551f463edc1a64cd89139
Simon <simonfong6@gmail.com>
2019-10-13 19:29:33 -0700

    Working proto

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# uwu-links
+# uwu links
 Shortlinks
+
+# How to use on your browser
+Go to [chrome://settings/searchEngines?search=search](chrome://settings/searchEngines?search=search)
+
+Add Other Search Engine Button
+
+## Search engine
+```
+uwu
+```
+
+## Keyword
+```
+uw
+```
+
+## URL with %s in place of query
+```
+http://uwu.mothakes.com/go/%s
+```
+
+
+# Development
+Run the server by:
+```
+python3 server.py -d
+```
---
+++ uwu_links/server.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python2
+"""
+Server code to track number of visitors.
+"""
+import logging
+import json
+from flask import Flask
+from flask import request
+from flask import send_from_directory
+from flask import redirect
+from flask import jsonify
+from flask import render_template
+from flask import url_for
+
+
+app = Flask(__name__)
+
+
+class Links(dict):
+    pass
+
+
+links = Links()
+
+
+@app.route('/')
+def index():
+
+    return send_from_directory('.', 'index.html')
+
+@app.route('/go/<short_link>')
+def go(short_link):
+    global links
+    if short_link not in links:
+        print(links)
+        return redirect(url_for('add', short_link=short_link))
+    else:
+        return redirect(links[short_link])
+
+@app.route('/add/<short_link>', methods=['GET', 'POST'])
+def add(short_link):
+    global links
+    if request.method == 'GET':
+        return render_template('add.jinja', short_link=short_link)
+    elif request.method == 'POST':
+        url = request.form['url']
+        links[short_link] = url
+        print("Adding {}".format(url))
+        return redirect(url_for('go', short_link=short_link))
+
+
+
+def main(args):
+
+    app.run(
+        host='0.0.0.0',
+        debug=args.debug,
+        port=args.port)
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+
+    parser.add_argument('-p', '--port',
+                        help="Port that the server will run on.",
+                        type=int,
+                        default=8080)
+    parser.add_argument('-d', '--debug',
+                        help="Whether or not to run in debug mode.",
+                        default=False,
+                        action='store_true')
+    parser.add_argument('--prod',
+                        help="Whether or not to run in prod mode.",
+                        default=False,
+                        action='store_true')
+
+    parser.add_argument('--no_log',
+                        help="Whether to not keep logs.",
+                        default=False,
+                        action='store_true')
+
+    args = parser.parse_args()
+    main(args)
